b5b999b5ca091dc4e574686bb80c392ba953f7b0
ikgron <boogie-rover.0a@icloud.com>
2024-06-06 10:38:09 +0200

    deleted curly brace

--- src/Report.jsx
+++ src/Report.jsx
@@ -4,7 +4,7 @@ import PDFsheet from "./Reports/ReportGen.jsx";
 function Report() {
     return (
         <Container className="reportContainer">
-            <PDFsheet sections={["{ROE: Authorized to use deadly force on any personel that interferes with operation to locate and neutralize target", "TEA: Target Engagement Authority for this target is Mongol Actual", "SPINS: Be sure to use route Beetlejuice", "IDP: friendlies are marked green, allies are marked blue, enemies are marked red:"]} />
+            <PDFsheet sections={["ROE: Authorized to use deadly force on any personel that interferes with operation to locate and neutralize target", "TEA: Target Engagement Authority for this target is Mongol Actual", "SPINS: Be sure to use route Beetlejuice", "IDP: friendlies are marked green, allies are marked blue, enemies are marked red:"]} />
         </Container>
 
     );
